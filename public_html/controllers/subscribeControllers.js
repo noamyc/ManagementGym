ed892f4201cef9cbc00b753cba4b37e036ba04d1
Lorenzo <lrgazzella@gmail.com>
2015-07-18 19:17:19 +0200

    Inizio view login

--- public_html/controllers/subscribeControllers.js
+++ public_html/controllers/subscribeControllers.js
@@ -1,7 +1,28 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
+/* global angular */
+
+angular.module("urlCtrl")
+.controller("loginCtrl", function($scope, $http){
+    
+    $http.get("http://localhost:5500/users")
+    .success(function(data){
+        $scope.users = data;
+        console.log($scope.users);
+    }).error(function(error){
+        $scope.error = error;
+    });
+    
+    $scope.accedi = function(user, pass){     
+        
+        $http.post("http://localhost:5500/users", {
+            username: user,
+            password: pass
+        }).success(function (data) {
+            console.log("autenticato")
+        }).error(function (error) {
+            $scope.authenticationError = error;
+        });
+        
+    }; 
+});

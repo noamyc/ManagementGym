8bcff5f5b7556090f491118dbb92d5a73fa7fc9e
Lorenzo <lrgazzella@gmail.com>
2015-07-19 12:51:59 +0200

    Finita view login

--- public_html/controllers/app.js
+++ public_html/controllers/app.js
@@ -3,6 +3,15 @@ angular.module("urlCtrl", ["ngRoute"])
     $routeProvider.when("/login", {
         templateUrl: "views/login.html"
     });
+    $routeProvider.when("/admin", {
+        templateUrl: "views/admin.html"
+    });
+    $routeProvider.when("/user", {
+        templateUrl: "views/user.html"
+    });
+    $routeProvider.when("/subscribe", {
+        templateUrl: "views/subscribe.html"
+    });
     $routeProvider.otherwise({
         templateUrl: "views/login.html"
     });
---
+++ public_html/controllers/loginControllers.js
@@ -0,0 +1,25 @@
+/* global angular */
+
+angular.module("urlCtrl")
+.constant("userUrl", "http://localhost:5500/users/login")
+.controller("loginCtrl", function($scope, $http, userUrl, $location){
+     
+    $scope.accedi = function(user, pass){
+        
+        $http.post(userUrl, {
+            username: user,
+            password: pass
+        }, {
+            withCredentials: true
+        }).success(function (data) {
+            if(user == "admin"){
+                $location.path("/admin");
+            }else{
+                $location.path("/user");
+            }
+        }).error(function (error) {
+            $scope.authenticationError = error;
+        });
+        
+    }; 
+});

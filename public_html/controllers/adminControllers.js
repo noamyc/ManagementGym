f62f110bd88de555f5c23100d9fc7c369f3dde88
Lorenzo <lrgazzella@gmail.com>
2015-07-23 17:58:32 +0200

    Iniziata view admin

--- public_html/controllers/adminControllers.js
+++ public_html/controllers/adminControllers.js
@@ -1,7 +1,23 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+angular.module("urlCtrl")
+.constant("userUrl", "http://localhost:5500/users")
+.constant("logoutUrl", "http://localhost:5500/users/logout")
+.controller("adminCtrl", function($scope, $http, userUrl, logoutUrl, getData, $location){
+    
+    $scope.userSelected;
+    
+    $http.get(userUrl)
+    .success(function(data){
+        $scope.users = data;
+        console.log(data);
+    }).error(function(error){
+        $scope.error = error;
+    });
+    
+    $scope.selectUser = function(user){
+        $scope.userSelected = user;
+    };
+     
+     
+    
+});
 
